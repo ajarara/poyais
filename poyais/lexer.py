8152f3a7de61fb6b1fea70045fa30d3417a0e098
Ahmad Jarara <ajarara@jarmac.org>
2017-12-16 19:06:46 -0500

    Lex strings
    This _looks_ good, but if we want to put in a literal " it needs to be
    quoted.
    
    There are a couple of problems here, the first is that we iterate one
    by one, unless we hit a symbol in which case we jump.
    
    One nice feature I'd like is the ability to do a triple quote to evade
    this issue.

--- poyais/lexer.py
+++ poyais/lexer.py
@@ -1,3 +1,4 @@
+import re
 
 
 # maybe someday I'll add in support for brackets...
@@ -10,3 +11,63 @@ def lex(program_string, space_chars={'`', '(', ')', "'"}):
         else:
             out.append(char)
     return "".join(out).split()
+
+# now that's okay, but it doesn't support strings at all.
+# instead we can explicitly define what tokens are, consume them
+# and move on throughout the program.
+# really the only thing that has state that must be preserved past
+# whitespace is a string, and we delimit these by double apostrophes:
+#
+#   "
+#
+# so once we encounter these we'll search until the next unescaped string
+# but this changes our norvig inspired lexer into an iterator
+
+# the only long lived state that persists across line breaks are string_states.
+# during input we'll strip newlines and carriage returns, so no big deal.
+
+
+def lex(program_string,
+        string_delim="'",
+        token_chars={'`', '(', ')', "'"},
+        whitespace={'\t', ' '},
+        symbol_reg=re.compile(r"[a-zA-Z\-][a-zA-Z\-0-9]*")):
+    pos = 0
+    buf = []
+    state_string = False
+    state_escaped = False
+
+    while pos < len(program_string):
+        char = program_string[pos]
+        if state_string:
+            if char == '"':
+                buf.append(char)
+                if state_escaped:
+                    state_escaped = False
+                else:
+                    state_string = False
+                    yield "".join(buf)
+                    buf = []
+            elif char == "\\":
+                if state_escaped:
+                    buf.append(char)
+                else:
+                    state_escaped = True
+            else:
+                buf.append(char)
+        elif char == '"':
+            state_string = True
+            buf.append(char)
+        elif char == "\\":
+            raise ValueError(
+                "Backslash outside of string context at {}".format(pos))
+        elif char in token_chars:
+            yield char
+        elif symbol_reg.match(program_string[pos:]):
+            match = symbol_reg.match(program_string[pos:]).group()
+            pos += len(match) - 1
+            yield match
+        elif char not in whitespace:
+            raise ValueError(
+                "Uncaught string at pos " + str(pos))
+        pos += 1
