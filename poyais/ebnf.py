272a35ec503a4ad9854235591242f5d19f33bbbc
Ahmad Jarara <ajarara@jarmac.org>
2018-01-09 22:34:09 -0500

    yield lexedRule with lhs on lhs
    who woulda thought

--- poyais/ebnf.py
+++ poyais/ebnf.py
@@ -130,8 +130,4 @@ def errmsg_rule(why, rule):
 
 def ebnf_lexer(ebnf_string):
     for rule in split_into_rules(ebnf_string):
-        yield LexedRule(rule.rhs, tuple(lex_rule(rule)))
-
-
-# but this isn't enough, we want more than a tokenized stream, instead we want an AST. 
-
+        yield LexedRule(rule.lhs, tuple(lex_rule(rule)))
