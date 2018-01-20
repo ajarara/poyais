d89fbf32725a6b87aae8930e19579aa97578a558
Ahmad Jarara <ajarara@jarmac.org>
2018-01-19 22:38:33 -0500

    Add BST impl
    Quick hack for interval tree

--- poyais/utility.py
+++ poyais/utility.py
@@ -14,3 +14,29 @@ def memoize(fun):
         else:
             return cache[hashable_params]
     return memoized
+
+
+# to be used for a quick interval tree
+class BSTNode:
+
+    def __init__(self, val, left=None, right=None):
+        for arg in (left, right):
+            assert(arg is None or isinstance(arg, BSTNode))
+
+        if left and right:
+            assert left.val <= right.val
+        self.left = left
+        self.right = right
+        self.val = val
+
+    def insert(self, val):
+        if val <= self.val:
+            if self.left is None:
+                self.left = BSTNode(val)
+            else:
+                self.left.insert(val)
+        elif val > self.val:
+            if self.right is None:
+                self.right = BSTNode(val)
+            else:
+                self.right.insert(val)
